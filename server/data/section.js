14c600f8f34433e4323c55765075ca6953422780
chenge <shawnnq@gmail.com>
2018-06-18 13:28:39 +0800

    db

--- server/data/section.js
+++ server/data/section.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const db = mongoose.createConnection('mongodb://106.15.203.222/torch', { promiseLibrary: Promise });
+const db = mongoose.createConnection('mongodb://127.0.0.1/torch', { promiseLibrary: Promise });
 
 const fieldSchema = new Schema({ caption: String, name: String, type: String, placeholder: String, value: Schema.Types.Mixed, list: Schema.Types.Mixed });
 const sectionSchema = new Schema({
